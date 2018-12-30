9f1150312dafa00446c82941cd25b7f928662581
Rodney McQuain <rodneymcquain95@gmail.com>
2018-12-30 18:33:54 -0500

    Initial Commit

---
+++ ClientApp/components/User/MyAccount.tsx
@@ -0,0 +1,19 @@
+﻿import * as React from 'react';
+import { logout } from '../../helpers/token';
+import { RouteComponentProps } from 'react-router';
+
+export class MyAccount extends React.Component<RouteComponentProps<{}>, {}> {
+    public render() {
+        return (
+            <div>
+                <h1>My Account</h1>
+                <button className="btn" onClick={ () => this.onClick_btLogout() } >Logout</button>
+            </div>
+        );
+    }
+
+    private onClick_btLogout() {
+        logout();
+        this.props.history.push(`/login`);
+    }
+}

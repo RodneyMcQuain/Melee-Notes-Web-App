0122fd2437e6ab37bcaf9b16310a2d6a1316d688
Rodney McQuain <rodneymcquain95@gmail.com>
2018-12-31 14:08:29 -0500

    Added paraenthesis to return statement in render

--- ClientApp/components/NavMenu.tsx
+++ ClientApp/components/NavMenu.tsx
@@ -1,55 +1,58 @@
 import * as React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { isAuthenticated } from '../helpers/token';
 
 export class NavMenu extends React.Component<{}, {}> {
     public render() {
-        return <div className='main-nav'>
+        return (
+            <div className='main-nav'>
                 <div className='navbar navbar-inverse'>
-                <div className='navbar-header'>
-                    <button type='button' className='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
-                        <span className='sr-only'>Toggle navigation</span>
-                        <span className='icon-bar'></span>
-                        <span className='icon-bar'></span>
-                        <span className='icon-bar'></span>
-                    </button>
-                    <Link className='navbar-brand' to={ '/' }>Melee Notes</Link>
-                </div>
-                <div className='clearfix'></div>
-                <div className='navbar-collapse collapse'>
-                    <ul className='nav navbar-nav'>
-                        <li>
-                            <NavLink to={ '/' } exact activeClassName='active'>
-                                <span className='glyphicon glyphicon-home'></span> Home
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/addTournament'} activeClassName='active'>
-                                <span className='glyphicon glyphicon-th-list'></span> Add Tournament
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/addSet'} activeClassName='active'>
-                                <span className='glyphicon glyphicon-th-list'></span> Add Set
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/players'} activeClassName='active'>
-                                <span className='glyphicon glyphicon-th-list'></span> Players
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/statistics'} activeClassName='active'>
-                                <span className='glyphicon glyphicon-th-list'></span> Statistics
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/myAccount'} activeClassName='active'>
-                                <span className='glyphicon glyphicon-th-list'></span> My Account
-                            </NavLink>
-                        </li>
-                    </ul>
+                    <div className='navbar-header'>
+                        <button type='button' className='navbar-toggle' data-toggle='collapse' data-target='.navbar-collapse'>
+                            <span className='sr-only'>Toggle navigation</span>
+                            <span className='icon-bar'></span>
+                            <span className='icon-bar'></span>
+                            <span className='icon-bar'></span>
+                        </button>
+                        <Link className='navbar-brand' to={ '/' }>Melee Notes</Link>
+                    </div>
+                    <div className='clearfix'></div>
+                    <div className='navbar-collapse collapse'>
+                        <ul className='nav navbar-nav'>
+                            <li>
+                                <NavLink to={ '/' } exact activeClassName='active'>
+                                    <span className='glyphicon glyphicon-home'></span> Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'/addTournament'} activeClassName='active'>
+                                    <span className='glyphicon glyphicon-th-list'></span> Add Tournament
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'/addSet'} activeClassName='active'>
+                                    <span className='glyphicon glyphicon-th-list'></span> Add Set
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'/players'} activeClassName='active'>
+                                    <span className='glyphicon glyphicon-th-list'></span> Players
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'/statistics'} activeClassName='active'>
+                                    <span className='glyphicon glyphicon-th-list'></span> Statistics
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={'/myAccount'} activeClassName='active'>
+                                    <span className='glyphicon glyphicon-th-list'></span> My Account
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>;
+        );
     }
 }

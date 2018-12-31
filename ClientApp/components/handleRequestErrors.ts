2c09466a8951da0c19e8ddc605322507aa266e7a
Rodney McQuain <rodneymcquain95@gmail.com>
2018-12-31 11:17:03 -0500

    Added check for 401 and added route for unhandled error

--- ClientApp/components/handleRequestErrors.ts
+++ ClientApp/components/handleRequestErrors.ts
@@ -3,12 +3,19 @@
 export const handleResponse = (history: any, response: any) => {
     let responseStatus = response.status;
 
-    if (responseStatus === 404) {
+    if (responseStatus === 401) {
+        history.push('/unauthorizedError');
+        throw new Error("401");
+    } else if (responseStatus === 404) {
         history.push('/notFoundError');
         throw new Error("404");
     } else if (responseStatus >= 500) {
         history.push('/serverError');
-        throw new Error(responseStatus);
-    } else
+        throw new Error(responseStatus.toString());
+    } else if (response.ok) { // If response is ok, return the response
         return response;
+    } else {
+        history.push('/error');
+        throw new Error("Unhandled " + responseStatus);
+    }
 }

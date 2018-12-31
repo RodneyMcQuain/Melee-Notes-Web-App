e778baa230ed7e5e3cdb6626f43e335f3bfaaa76
Rodney McQuain <rodneymcquain95@gmail.com>
2018-12-31 13:36:59 -0500

    Moved out of components folder

---
+++ ClientApp/helpers/handleResponseErrors.ts
@@ -0,0 +1,19 @@
+﻿export const handleResponse = (history: any, response: any) => {
+    let responseStatus = response.status;
+
+    if (responseStatus === 401) {
+        history.push('/unauthorizedError');
+        throw new Error("401");
+    } else if (responseStatus === 404) {
+        history.push('/notFoundError');
+        throw new Error("404");
+    } else if (responseStatus >= 500) {
+        history.push('/serverError');
+        throw new Error(responseStatus.toString());
+    } else if (response.ok) { // If response is ok, return the response
+        return response;
+    } else {
+        history.push('/error');
+        throw new Error("Unhandled " + responseStatus);
+    }
+}

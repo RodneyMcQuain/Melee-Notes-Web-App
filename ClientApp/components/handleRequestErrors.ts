2b2a21496d22e1eba810e3254a20c41e17fdcdbd
Rodney McQuain <rodneymcquain95@gmail.com>
2018-12-30 23:23:58 -0500

    Added checks for 404 and server response status errors

--- ClientApp/components/handleRequestErrors.ts
+++ ClientApp/components/handleRequestErrors.ts
@@ -1,3 +1,14 @@
-﻿export const handleResponseStatus = (history: History, response: string) => {
-    
+﻿//import history from './history';
+
+export const handleResponse = (history: any, response: any) => {
+    let responseStatus = response.status;
+
+    if (responseStatus === 404) {
+        history.push('/notFoundError');
+        throw new Error("404");
+    } else if (responseStatus >= 500) {
+        history.push('/serverError');
+        throw new Error(responseStatus);
+    } else
+        return response;
 }

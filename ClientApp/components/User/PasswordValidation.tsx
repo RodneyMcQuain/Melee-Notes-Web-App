527e7ab4b9a30753bc2b503cd537c6b23e55c40e
Rodney McQuain <rodneymcquain95@gmail.com>
2018-12-31 21:08:42 -0500

    Initial Commit

---
+++ ClientApp/components/User/PasswordValidation.tsx
@@ -0,0 +1,40 @@
+﻿import * as React from 'react';
+import { IUser } from 'ClientApp/helpers/interfaces';
+import { RouteComponentProps } from 'react-router';
+
+interface PasswordValidationProps {
+    isPasswordMatch: boolean;
+    hasPasswordLength: boolean;
+    hasPasswordNumber: boolean;
+    hasPasswordLowercase: boolean;
+    hasPasswordUppercase: boolean;
+    hasPasswordSymbol: boolean;
+}
+
+export class PasswordValidation extends React.Component<PasswordValidationProps, {}> {
+    public render() {
+        const { isPasswordMatch, hasPasswordLength, hasPasswordNumber,
+            hasPasswordLowercase, hasPasswordUppercase, hasPasswordSymbol } = this.props;
+
+        let passwordValidationArray = [isPasswordMatch, hasPasswordLength, hasPasswordNumber,
+            hasPasswordLowercase, hasPasswordUppercase, hasPasswordSymbol];
+
+        let validationClassNames = passwordValidationArray.map(isValid => {
+            if (isValid)
+                return "valid";
+            else
+                return "invalid";
+        });
+
+        return (
+            <div>
+                <p className={ validationClassNames[0] }>Passwords Match</p>
+                <p className={ validationClassNames[1] }>Minimum of 8 Characters</p>
+                <p className={ validationClassNames[2] }>Number</p>
+                <p className={ validationClassNames[3] }>Lowercase Letter</p>
+                <p className={ validationClassNames[4] }>Uppercase Letter</p>
+                <p className={ validationClassNames[5] }>Symbol (?!@#$%^&*)</p>
+            </div>
+        );
+    }
+}

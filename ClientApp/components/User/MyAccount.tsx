9d8edf15a7190516fd7946f8a452219863cd5335
Rodney McQuain <rodneymcquain95@gmail.com>
2019-01-01 20:51:45 -0500

    Added details about the user

--- ClientApp/components/User/MyAccount.tsx
+++ ClientApp/components/User/MyAccount.tsx
@@ -1,12 +1,46 @@
 ﻿import * as React from 'react';
-import { logout } from '../../helpers/token';
+import { logout, getCurrentUserId, getAuthorizationHeaders } from '../../helpers/token';
 import { RouteComponentProps } from 'react-router';
+import { IUser } from '../../helpers/interfaces';
+import { handleResponse } from '../../helpers/handleResponseErrors';
+import { formatDate } from '../../helpers/formatDate';
 
-export class MyAccount extends React.Component<RouteComponentProps<{}>, {}> {
+interface MyAccountState {
+    user: IUser;
+    isLoading: boolean;
+}
+
+export class MyAccount extends React.Component<RouteComponentProps<{}>, MyAccountState> {
+    constructor() {
+        super();
+        this.state = {
+            user: {} as IUser,
+            isLoading: true
+        }
+    }
+
+    public componentDidMount() {
+        const userId = getCurrentUserId();
+
+        fetch(`api/User/${userId}`, { headers: getAuthorizationHeaders() })
+            .then(response => handleResponse(this.props.history, response))
+            .then(response => response.json() as Promise<IUser>)
+            .then(user => this.setState({
+                user: user,
+                isLoading: false
+            }))
+            .catch(error => console.log(error));
+    }
+
     public render() {
+        const user = this.state.user;
+
         return (
             <div>
                 <h1>My Account</h1>
+                <p>Username: { user.username }</p>
+                <p>Email: { user.email }</p>
+                <p>Date Created: { formatDate(user.dateCreated) }</p>
                 <button className="btn" onClick={ () => this.onClick_btLogout() } >Logout</button>
             </div>
         );

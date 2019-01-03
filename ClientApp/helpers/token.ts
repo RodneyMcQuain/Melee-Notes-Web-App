c75d4a943cf05a6433cca9529a092fd169a57c5a
Rodney McQuain <rodneymcquain95@gmail.com>
2019-01-02 21:13:01 -0500

    Refactored token into object literal
    All of the required functions and implementation is now in one object rather than scattered accross many public functions

--- ClientApp/helpers/token.ts
+++ ClientApp/helpers/token.ts
@@ -1,72 +1,92 @@
 ﻿import { IToken } from "./interfaces";
 
-export let isAuthenticated = false;
+export let Token = (function () {
+    let isAuthenticated = false;
+    let userId = "";
+    let token = {} as IToken;
+    let USER_LOCAL_STORAGE_NAME = "user"
 
-export const getCurrentUserId = () => {
-    return userId;
-}
+    const decodeToken = (token: string) => {
+        let base64Url = token.split('.')[1];
+        let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        let decodedToken = JSON.parse(window.atob(base64));
 
-export const getAuthorizationHeaders = () => {
-    return {
-        'Authorization': "Bearer " + getToken(),
-        'Content-Type': 'application/json'
-    };
-}
-
-export const getUser = (): IToken | null => {
-    let user: string | null = localStorage.getItem("user")
-
-    if (user) {
-        let userJson = JSON.parse(user);
-        return userJson;
+        return decodedToken;
     }
 
-    return null;
-}
+    const getUser = (): IToken | null => {
+        let user: string | null = localStorage.getItem("user")
 
-export const getToken = (): string | null => {
-    let user = getUser();
+        if (user) {
+            let userJson = JSON.parse(user);
+            return userJson;
+        }
 
-    if (user)
-        return user.token;
-
-    return null;
-}
-
-export const setToken = (token: IToken) => {
-    let tokenString = token.token;
-    let decodedToken = decodeToken(tokenString);
-
-    decodedToken.token = tokenString;
-
-    isAuthenticated = true;
-    userId = decodedToken.userId;
-
-    localStorage.setItem("user", JSON.stringify(decodedToken));
-}
-
-const decodeToken = (token: string) => {
-    let base64Url = token.split('.')[1];
-    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    let decodedToken = JSON.parse(window.atob(base64));
-
-    return decodedToken;
-}
-
-export const logout = () => {
-    isAuthenticated = false;
-
-    localStorage.removeItem("user");
-}
-
-let user = getUser();
-let userId = "";
-// Initialize userId if user token hasn't expired since last run of app
-if (user) {
-    if (decodeToken(user.token).exp < Date.now() / 1000)
-        localStorage.clear();
-    else {
-        userId = user.userId.toString();
-        isAuthenticated = true;
+        return null;
     }
-}
+
+    const getToken = (): string | null => {
+        let user = getUser();
+
+        if (user)
+            return user.token;
+
+        return null;
+    }
+
+    let TokenObj = {
+        setToken: function(token: IToken) {
+            let tokenString = token.token;
+            let decodedToken = decodeToken(tokenString);
+
+            decodedToken.token = tokenString;
+
+            isAuthenticated = true;
+            userId = decodedToken.userId;
+
+            localStorage.setItem(USER_LOCAL_STORAGE_NAME, JSON.stringify(decodedToken));
+        },
+
+        isUserAuthenticated: function() {
+            let user = getUser();
+
+            isAuthenticated = false;
+            if (user) {
+                if (decodeToken(user.token).exp < Date.now() / 1000) {
+                    localStorage.removeItem(USER_LOCAL_STORAGE_NAME);
+                    isAuthenticated = false;
+                    userId = "0";
+                } else {
+                    isAuthenticated = true;
+                }
+            }
+
+            return isAuthenticated;
+        },
+
+        getUserId: function() {
+            let user = getUser();
+
+            if (user) {
+                userId = user.userId.toString();
+            }
+
+            return userId;
+        },
+
+        getAuthorizationHeaders: function() {
+            return {
+                'Authorization': "Bearer " + getToken(),
+                'Content-Type': 'application/json'
+            };
+        },
+
+        logout: function() {
+            isAuthenticated = false;
+
+            localStorage.removeItem(USER_LOCAL_STORAGE_NAME);
+        }
+    }
+
+    return TokenObj;
+}());

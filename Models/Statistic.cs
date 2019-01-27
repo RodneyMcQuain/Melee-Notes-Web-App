2887af576c4f3da281642b6eef05a96c97a6d92e
Rodney McQuain <rodneymcquain95@gmail.com>
2019-01-27 12:32:16 -0500

    Initial Commit

---
+++ Models/Statistic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace melee_notes.Models
+{
+    public class Statistic
+    {
+        public string PlayerId { get; set; }
+        public string MyCharacter { get; set; }
+        public string OpponentCharacter { get; set; }
+        public string Format { get; set; }
+        public string Type { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+
+        public Statistic(string PlayerId, string MyCharacter, string OpponentCharacter, string Format, string Type, string StartDate, string EndDate)
+        {
+            this.PlayerId = PlayerId;
+            this.MyCharacter = MyCharacter;
+            this.OpponentCharacter = OpponentCharacter;
+            this.Format = Format;
+            this.Type = Type;
+            this.StartDate = StartDate;
+            this.EndDate = EndDate;
+        }
+    }
+}

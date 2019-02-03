b02a74ad0d8640e949288527b41e31fb1d57dd7c
Rodney McQuain <rodneymcquain95@gmail.com>
2019-02-02 20:27:54 -0500

    Abstracted stage + game count into renderStages function

--- ClientApp/components/Statistic/Statistics.tsx
+++ ClientApp/components/Statistic/Statistics.tsx
@@ -30,7 +30,7 @@ interface StatisticsState {
     setsLost: number;
     gamesWon: number[];
     gamesLost: number[];
-    isSetCountLoading: boolean;
+    isStatisticLoading: boolean;
     isAfterFirstSubmit: boolean;
     isLoading: boolean;
 }
@@ -44,7 +44,7 @@ export class Statistics extends React.Component<RouteComponentProps<{}>, Statist
         this.state = {
             statistic: {} as IStatistic,
             isLoading: true,
-            isSetCountLoading: false,
+            isStatisticLoading: false,
             isAfterFirstSubmit: false,
             dateDropdown: "All Time",
             setsWon: -1,
@@ -75,13 +75,7 @@ export class Statistics extends React.Component<RouteComponentProps<{}>, Statist
     }
 
     public render() {
-        const { statistic, setsWon, setsLost, gamesWon, gamesLost, isSetCountLoading, isAfterFirstSubmit } = this.state;
-        const BATTLEFIELD_ID = 0;
-        const DREAMLAND_ID = 1;
-        const YOSHIS_STORY_ID = 2;
-        const FOUNTAIN_OF_DREAMS_ID = 3;
-        const FINAL_DESTINATION_ID = 4;
-        const POKEMON_STADIUM_ID = 5;
+        const { statistic, setsWon, setsLost, gamesWon, gamesLost, isStatisticLoading, isAfterFirstSubmit } = this.state;
 
         const date = this.state.dateDropdown === "Specify Date"
             ? <div>
@@ -97,47 +91,11 @@ export class Statistics extends React.Component<RouteComponentProps<{}>, Statist
             </div>
             : <div></div>
         
-        const setCount = isSetCountLoading ? <ContentPreloader /> : <div className="col-xs-12" >Set Count: {setsWon}-{setsLost} (excludes character in query)</div>
+        const setCount = isStatisticLoading ? <ContentPreloader /> : <div className="col-xs-12" >Set Count: {setsWon}-{setsLost} (excludes character in query)</div>
 
         const afterFirstSubmit = isAfterFirstSubmit
             ? <div className="row statistic-negative-margin" >
                 { setCount }
-
-                <div className="img-padding-margin col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                    <img className="stage" src={require('../../images/Battlefield.jpg')}></img>
-                    <div className="-center-container">{ gamesWon[BATTLEFIELD_ID] } - { gamesLost[BATTLEFIELD_ID] }</div>
-                    <div className="-center-container">{ this.calculateWinRate(gamesWon[BATTLEFIELD_ID], gamesLost[BATTLEFIELD_ID]) }%</div>
-                </div>
-
-                <div className="img-padding-margin col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                    <img className="stage" src={require('../../images/Dreamland.jpg')}></img>
-                    <div className="-center-container">{ gamesWon[DREAMLAND_ID] } - { gamesLost[DREAMLAND_ID] }</div>
-                    <div className="-center-container">{ this.calculateWinRate(gamesWon[DREAMLAND_ID], gamesLost[DREAMLAND_ID]) }%</div>
-                </div>
-
-                <div className="img-padding-margin col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                    <img className="stage" src={require('../../images/FinalDestination.jpg')}></img>
-                    <div className="-center-container">{ gamesWon[FINAL_DESTINATION_ID] } - { gamesLost[FINAL_DESTINATION_ID] }</div>
-                    <div className="-center-container">{ this.calculateWinRate(gamesWon[FINAL_DESTINATION_ID], gamesLost[FINAL_DESTINATION_ID]) }%</div>
-                </div>
-
-                <div className="img-padding-margin col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                    <img className="stage" src={require('../../images/FountainOfDreams.jpg')}></img>
-                    <div className="-center-container">{ gamesWon[FOUNTAIN_OF_DREAMS_ID] } - { gamesLost[FOUNTAIN_OF_DREAMS_ID] }</div>
-                    <div className="-center-container">{ this.calculateWinRate(gamesWon[FOUNTAIN_OF_DREAMS_ID], gamesLost[FOUNTAIN_OF_DREAMS_ID]) }%</div>
-                </div>
-
-                <div className="img-padding-margin col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                    <img className="stage" src={require('../../images/PokemonStadium.jpg')}></img>
-                    <div className="-center-container">{ gamesWon[POKEMON_STADIUM_ID] } - { gamesLost[POKEMON_STADIUM_ID] }</div>
-                    <div className="-center-container">{ this.calculateWinRate(gamesWon[POKEMON_STADIUM_ID], gamesLost[POKEMON_STADIUM_ID]) }%</div>
-                </div>
-
-                <div className="img-padding-margin col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                    <img className="stage" src={require('../../images/YoshisIsland.jpg')}></img>
-                    <div className="-center-container">{ gamesWon[YOSHIS_STORY_ID] } - { gamesLost[YOSHIS_STORY_ID] }</div>
-                    <div className="-center-container">{ this.calculateWinRate(gamesWon[YOSHIS_STORY_ID], gamesLost[YOSHIS_STORY_ID]) }%</div>
-                </div>
                    
             </div>
            : <div></div>
@@ -192,6 +150,55 @@ export class Statistics extends React.Component<RouteComponentProps<{}>, Statist
         );
     }
 
+    private renderStages(gamesWon: number[], gamesLost: number[]) {
+        const BATTLEFIELD_ID = 0;
+        const DREAMLAND_ID = 1;
+        const YOSHIS_STORY_ID = 2;
+        const FOUNTAIN_OF_DREAMS_ID = 3;
+        const FINAL_DESTINATION_ID = 4;
+        const POKEMON_STADIUM_ID = 5;
+
+        return (
+            <div>
+                <div className="img-padding-margin col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <img className="stage" src={require('../../images/Battlefield.jpg')}></img>
+                    <div className="-center-container">{ gamesWon[BATTLEFIELD_ID] } - { gamesLost[BATTLEFIELD_ID] }</div>
+                    <div className="-center-container">{ this.calculateWinRate(gamesWon[BATTLEFIELD_ID], gamesLost[BATTLEFIELD_ID]) }%</div>
+                </div>
+
+                <div className="img-padding-margin col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <img className="stage" src={require('../../images/Dreamland.jpg')}></img>
+                    <div className="-center-container">{ gamesWon[DREAMLAND_ID] } - { gamesLost[DREAMLAND_ID] }</div>
+                    <div className="-center-container">{ this.calculateWinRate(gamesWon[DREAMLAND_ID], gamesLost[DREAMLAND_ID]) }%</div>
+                </div>
+
+                <div className="img-padding-margin col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <img className="stage" src={require('../../images/FinalDestination.jpg')}></img>
+                    <div className="-center-container">{ gamesWon[FINAL_DESTINATION_ID] } - { gamesLost[FINAL_DESTINATION_ID] }</div>
+                    <div className="-center-container">{ this.calculateWinRate(gamesWon[FINAL_DESTINATION_ID], gamesLost[FINAL_DESTINATION_ID]) }%</div>
+                </div>
+
+                <div className="img-padding-margin col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <img className="stage" src={require('../../images/FountainOfDreams.jpg')}></img>
+                    <div className="-center-container">{ gamesWon[FOUNTAIN_OF_DREAMS_ID] } - { gamesLost[FOUNTAIN_OF_DREAMS_ID] }</div>
+                    <div className="-center-container">{ this.calculateWinRate(gamesWon[FOUNTAIN_OF_DREAMS_ID], gamesLost[FOUNTAIN_OF_DREAMS_ID]) }%</div>
+                </div>
+
+                <div className="img-padding-margin col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <img className="stage" src={require('../../images/PokemonStadium.jpg')}></img>
+                    <div className="-center-container">{ gamesWon[POKEMON_STADIUM_ID] } - { gamesLost[POKEMON_STADIUM_ID] }</div>
+                    <div className="-center-container">{ this.calculateWinRate(gamesWon[POKEMON_STADIUM_ID], gamesLost[POKEMON_STADIUM_ID]) }%</div>
+                </div>
+
+                <div className="img-padding-margin col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <img className="stage" src={require('../../images/YoshisIsland.jpg')}></img>
+                    <div className="-center-container">{ gamesWon[YOSHIS_STORY_ID] } - { gamesLost[YOSHIS_STORY_ID] }</div>
+                    <div className="-center-container">{ this.calculateWinRate(gamesWon[YOSHIS_STORY_ID], gamesLost[YOSHIS_STORY_ID]) }%</div>
+                </div>
+            </div>
+        );
+    }
+
     private calculateWinRate(wonCount: number, lostCount: number) {
         let totalGames = wonCount + lostCount;
 
@@ -220,7 +227,7 @@ export class Statistics extends React.Component<RouteComponentProps<{}>, Statist
         let userId = Token.getUserId();
         this.setState({
             isAfterFirstSubmit: true,
-            isSetCountLoading: true
+            isStatisticLoading: true
         });
 
         let setsWon = 0;
@@ -240,7 +247,7 @@ export class Statistics extends React.Component<RouteComponentProps<{}>, Statist
                 setsLost: setsLost,
                 gamesWon: gamesWon,
                 gamesLost: gamesLost,
-                isSetCountLoading: false
+                isStatisticLoading: false
             }))
             .catch(error => console.log(error));
     }

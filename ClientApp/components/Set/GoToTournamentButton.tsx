d1a0748c59733cbe1e7cb795c13048af817668f9
Rodney McQuain <rodneymcquain95@gmail.com>
2019-02-10 15:26:27 -0500

    Initial Commit

---
+++ ClientApp/components/Set/GoToTournamentButton.tsx
@@ -0,0 +1,16 @@
+﻿import * as React from "react";
+import { History } from 'history';
+
+interface IGoToTournamentButtonProps {
+    tournamentId: number;
+    history: History;
+}
+
+export const GoToTournamentButton = (props: IGoToTournamentButtonProps) => {
+    const { tournamentId, history } = props;
+    const onClick_btGoToTournament = (tournamentId: number) => {
+        history.push(`/tournament/${tournamentId}`);
+    }
+
+    return <button className="btn" onClick={() => onClick_btGoToTournament(tournamentId)} >Go Back to Tournament</button>
+}

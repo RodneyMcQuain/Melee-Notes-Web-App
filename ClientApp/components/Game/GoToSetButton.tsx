49048ef74c449a12c7c5a621ca3558a3ca7d9c6f
Rodney McQuain <rodneymcquain95@gmail.com>
2019-02-10 14:39:36 -0500

    Initial Commit

---
+++ ClientApp/components/Game/GoToSetButton.tsx
@@ -0,0 +1,18 @@
+﻿import * as React from "react";
+import { History } from 'history';
+
+interface IGoToSetButtonProps {
+    tournamentId: number;
+    setId: number;
+    history: History;
+
+}
+
+export const GoToSetButton = (props: IGoToSetButtonProps) => {
+    const { tournamentId, setId, history } = props;
+    const onClick_btGoToSet = (tournamentId: number, setId: number) => {
+        history.push(`/tournament/${tournamentId}/set/${setId}`);    
+    }
+
+    return <button className="btn" onClick={() => onClick_btGoToSet(tournamentId, setId)} >Go Back to Set</button>
+}

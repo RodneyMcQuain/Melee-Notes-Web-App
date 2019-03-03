ab51ee61861f7b09df7d88f9a94c0a82f6151b4b
Rodney McQuain <rodneymcquain95@gmail.com>
2019-03-03 13:11:58 -0500

    Added moduleNameMapper for static asset mocking

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 ﻿module.exports = {
+    "moduleNameMapper": {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/ClientApp/tests/mocks/fileMock.ts",
+        "\\.(css|pcss)$": "<rootDir>/ClientApp/tests/mocks/styleMock.ts"
+    },
     "roots": [
         "<rootDir>/ClientApp"
     ],

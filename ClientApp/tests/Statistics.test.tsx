0e085b4e89569fd1c4ea0ae4827b4df7299f3b99
Rodney McQuain <rodneymcquain95@gmail.com>
2019-03-03 16:10:12 -0500

    Improved formatting with describe and it

--- ClientApp/tests/Statistics.test.tsx
+++ ClientApp/tests/Statistics.test.tsx
@@ -6,25 +6,27 @@ import { Statistics } from '../components/Statistic/Statistics';
 
 configure({ adapter: new ReactSixteenAdapter() });
 
-const mock: any = jest.fn();
-const mockPropStatistics = shallow<Statistics>(<Statistics
-    match={ mock }
-    location={ mock }
-    history={ mock }
-/>);
+describe("Calculate Win Rate", () => {
+    const mock: any = jest.fn();
+    const mockPropStatistics = shallow<Statistics>(<Statistics
+        match={mock}
+        location={mock}
+        history={mock}
+    />);
 
-test('win rate with 10 wins and 5 loses', () => {
-    expect(mockPropStatistics.instance().calculateWinRate(10, 5)).toBe("66.67");
-});
+    it("calculates win rate with 10 wins and 5 loses", () => {
+        expect(mockPropStatistics.instance().calculateWinRate(10, 5)).toBe("66.67");
+    });
 
-test('win rate with 10 wins and 0 loses', () => {
-    expect(mockPropStatistics.instance().calculateWinRate(10, 0)).toBe("100.00");
-});
+    it("calculates win rate with 10 wins and 0 loses", () => {
+        expect(mockPropStatistics.instance().calculateWinRate(10, 0)).toBe("100.00");
+    });
 
-test('win rate with 0 wins and 10 loses', () => {
-    expect(mockPropStatistics.instance().calculateWinRate(0, 10)).toBe("0.00");
-});
+    it("calculates win rate with 0 wins and 10 loses", () => {
+        expect(mockPropStatistics.instance().calculateWinRate(0, 10)).toBe("0.00");
+    });
 
-test('win rate with 0 wins and 0 loses', () => {
-    expect(mockPropStatistics.instance().calculateWinRate(0, 0)).toBe("0.00");
+    it("calculates win rate with 0 wins and 0 loses", () => {
+        expect(mockPropStatistics.instance().calculateWinRate(0, 0)).toBe("0.00");
+    });
 });

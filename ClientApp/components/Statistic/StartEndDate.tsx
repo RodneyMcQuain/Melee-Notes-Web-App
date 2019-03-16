c83665e579cd5d4720ea01be96a57f60a618b72b
Rodney McQuain <rodneymcquain95@gmail.com>
2019-03-16 18:43:40 -0400

    Initial Commit

---
+++ ClientApp/components/Statistic/StartEndDate.tsx
@@ -0,0 +1,19 @@
+﻿import * as React from 'react';
+
+interface StartEndDateProps {
+    startDate: string;
+    endDate: string;
+    handleFieldChange: Function;
+}
+
+export const StartEndDate = (props: StartEndDateProps) => <>
+    <div className="statistic-form-margin form-group col-lg-4 col-md-4 col-sm-6 col-xs-12">
+        <label htmlFor="startDate" >Start Date</label>
+        <input type="date" name="startDate" className="form-control" value={ props.startDate } onChange={ e => props.handleFieldChange(e) } />
+    </div>
+
+    <div className="statistic-form-margin form-group col-lg-4 col-md-4 col-sm-6 col-xs-12">
+        <label htmlFor="endDate" >End Date</label>
+        <input type="date" name="endDate" className="form-control" value={ props.endDate } onChange={ e => props.handleFieldChange(e) } />
+    </div>
+</>

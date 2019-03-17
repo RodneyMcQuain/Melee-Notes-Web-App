d0b89f248556ab9478e91c1bdcf2e448cb969806
Rodney McQuain <rodneymcquain95@gmail.com>
2019-03-17 00:44:45 -0400

    Moved state to parent, Statistics, making this component stateless

--- ClientApp/components/Statistic/StatisticDropdowns.tsx
+++ ClientApp/components/Statistic/StatisticDropdowns.tsx
@@ -4,79 +4,62 @@ import { PlayerDropdown } from '../Dropdowns/PlayerDropdown';
 import { FormatDropdown } from '../Dropdowns/FormatDropdown';
 import { TypeDropdown } from '../Dropdowns/TypeDropdown';
 import { StartEndDate } from './StartEndDate';
-import { IStatistic } from '../../../helpers/interfaces';
+import { IStatistic } from '../../helpers/interfaces';
 
 interface StatisticDropdownsProps {
     statistic: IStatistic;
-    handleFieldChange: Function;
-}
-
-interface StatisticDropdownsState {
     dateDropdown: string;
+    handleFieldChange: Function;
+    handleDateDropdownChange: Function;
 }
 
-export class StatisticDropdowns extends React.Component<StatisticDropdownsProps, StatisticDropdownsState> {
-    constructor(props: StatisticDropdownsProps) {
-        super(props);
+export const StatisticDropdowns = (props: StatisticDropdownsProps) => {
+    const { statistic, dateDropdown, handleFieldChange, handleDateDropdownChange } = props;
 
-        this.state = {
-            dateDropdown: "All Time"
-        };
-    }
+    const date = dateDropdown === "Specify Date"
+        ? <StartEndDate
+            startDate={statistic.startDate}
+            endDate={statistic.endDate}
+            handleFieldChange={handleFieldChange}
+        />
+        : <div></div>
 
-    public render() {
-        const dateDropdown = this.state.dateDropdown;
-        const { statistic, handleFieldChange } = this.props;
+    return (
+        <>
+            <div className="statistic-form-margin form-group col-lg-4 col-md-4 col-sm-6 col-xs-12" >
+                <label htmlFor="myCharacter" >My Character</label>
+                <CharacterDropdown handleFieldChange={handleFieldChange} character={statistic.myCharacter} characterType="myCharacter" hasAll={true} />
+            </div>
 
-        const date = dateDropdown === "Specify Date"
-            ? <StartEndDate
-                startDate={statistic.startDate}
-                endDate={statistic.endDate}
-                handleFieldChange={handleFieldChange}
-            />
-            : <div></div>
+            <div className="statistic-form-margin form-group col-lg-4 col-md-4 col-sm-6 col-xs-12" >
+                <label htmlFor="oppponentCharacter" >Opponent Character</label>
+                <CharacterDropdown handleFieldChange={handleFieldChange} character={statistic.opponentCharacter} characterType="opponentCharacter" hasAll={true} />
+            </div>
 
-        return (
-            <>
-                <div className="statistic-form-margin form-group col-lg-4 col-md-4 col-sm-6 col-xs-12" >
-                    <label htmlFor="myCharacter" >My Character</label>
-                    <CharacterDropdown handleFieldChange={handleFieldChange} character={statistic.myCharacter} characterType="myCharacter" hasAll={true} />
-                </div>
+            <div className="statistic-form-margin form-group col-lg-4 col-md-4 col-sm-6 col-xs-12" >
+                <label htmlFor="player" >Player</label>
+                <PlayerDropdown handleFieldChange={handleFieldChange} playerId={statistic.playerId} hasAll={true} />
+            </div>
 
-                <div className="statistic-form-margin form-group col-lg-4 col-md-4 col-sm-6 col-xs-12" >
-                    <label htmlFor="oppponentCharacter" >Opponent Character</label>
-                    <CharacterDropdown handleFieldChange={handleFieldChange} character={statistic.opponentCharacter} characterType="opponentCharacter" hasAll={true} />
-                </div>
+            <div className="statistic-form-margin form-group col-lg-4 col-md-4 col-sm-6 col-xs-12" >
+                <label htmlFor="format" >Format</label>
+                <FormatDropdown handleFieldChange={handleFieldChange} format={statistic.format} hasAll={true} />
+            </div>
 
-                <div className="statistic-form-margin form-group col-lg-4 col-md-4 col-sm-6 col-xs-12" >
-                    <label htmlFor="player" >Player</label>
-                    <PlayerDropdown handleFieldChange={handleFieldChange} playerId={statistic.playerId} hasAll={true} />
-                </div>
+            <div className="statistic-form-margin form-group col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <label htmlFor="type" >Type</label>
+                <TypeDropdown handleFieldChange={handleFieldChange} type={statistic.type} hasAll={true} />
+            </div>
 
-                <div className="statistic-form-margin form-group col-lg-4 col-md-4 col-sm-6 col-xs-12" >
-                    <label htmlFor="format" >Format</label>
-                    <FormatDropdown handleFieldChange={handleFieldChange} format={statistic.format} hasAll={true} />
-                </div>
+            <div className="statistic-form-margin form-group col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                <label htmlFor="dateType" >Date</label>
+                <select name="dateType" className="form-control" value={dateDropdown} onChange={e => handleDateDropdownChange(e)} >
+                    <option value="All Time">All Time</option>
+                    <option value="Specify Date">Specify Date</option>
+                </select>
+            </div>
 
-                <div className="statistic-form-margin form-group col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                    <label htmlFor="type" >Type</label>
-                    <TypeDropdown handleFieldChange={handleFieldChange} type={statistic.type} hasAll={true} />
-                </div>
-
-                <div className="statistic-form-margin form-group col-lg-4 col-md-4 col-sm-6 col-xs-12">
-                    <label htmlFor="dateType" >Date</label>
-                    <select name="dateType" className="form-control" value={dateDropdown} onChange={e => this.handleDateDropdownChange(e)} >
-                        <option value="All Time">All Time</option>
-                        <option value="Specify Date">Specify Date</option>
-                    </select>
-                </div>
-
-                { date }
-            </>
-        );
-    }
-
-    private handleDateDropdownChange(event: React.ChangeEvent<HTMLSelectElement>) {
-        this.setState({ dateDropdown: event.target.value });
-    }
+            { date }
+        </>
+    );
 }

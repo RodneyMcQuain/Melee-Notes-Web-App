f4221dd6ef0eab3544bcd8f520095d1bdca5516c
Rodney McQuain <rodneymcquain95@gmail.com>
2019-04-01 15:10:35 -0400

    Initial Commit

---
+++ ClientApp/components/Statistic/SetRatio.tsx
@@ -0,0 +1,12 @@
+﻿import * as React from 'react';
+
+interface SetRatioProps {
+    setsWon: number;
+    setsLost: number;
+}
+
+export const SetRatio = (props: SetRatioProps) => (
+    <div className="col-xs-12">
+        Set Count: { props.setsWon }-{ props.setsLost } (excludes character in query)
+    </div>
+);

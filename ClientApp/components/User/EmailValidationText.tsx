10b22d249ca55e4fdf148766d19b6c1c52634083
Rodney McQuain <rodneymcquain95@gmail.com>
2020-03-04 22:59:41 -0500

    Added email and username validation text

---
+++ ClientApp/components/User/EmailValidationText.tsx
@@ -0,0 +1,24 @@
+﻿import * as React from 'react';
+
+interface EmailValidationTextProps {
+    isEmailNotEmpty: boolean;
+    isEmailValid: boolean;
+    isEmailDuplicate: boolean;
+    setArrayValidity: Function;
+}
+
+export const EmailValidationText = (props: EmailValidationTextProps) => {
+    const { isEmailNotEmpty, isEmailValid, isEmailDuplicate } = props;
+
+    let emailValidationArray = [isEmailNotEmpty, isEmailValid, isEmailDuplicate];
+
+    const validationClassNames = props.setArrayValidity(emailValidationArray);
+
+    return (
+        <>
+            <p className={ validationClassNames[0] }>Email is not empty</p>
+            <p className={ validationClassNames[1] }>Email is valid</p>
+            <p className={ validationClassNames[2] }>Email is available</p>
+        </>
+    );
+};
---
+++ ClientApp/components/User/UsernameValidationText.tsx
@@ -0,0 +1,22 @@
+﻿import * as React from 'react';
+
+interface UsernameValidationTextProps {
+    isUsernameNotEmpty: boolean;
+    isUsernameDuplicate: boolean;
+    setArrayValidity: Function;
+}
+
+export const UsernameValidationText = (props: UsernameValidationTextProps) => {
+    const { isUsernameNotEmpty, isUsernameDuplicate} = props;
+
+    let usernameValidationArray = [isUsernameNotEmpty, isUsernameDuplicate];
+
+    const validationClassNames = props.setArrayValidity(usernameValidationArray);
+
+    return (
+        <>
+            <p className={ validationClassNames[0] }>Username is not empty</p>
+            <p className={ validationClassNames[1] }>Username is available</p>
+        </>
+    );
+};
